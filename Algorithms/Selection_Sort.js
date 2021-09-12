4733164c818453b0839903ac638a26b925c21b6c
siddharth2411 <43139012+siddharth2411@users.noreply.github.com>
2021-09-13 00:01:32 +0530

    Update Selection_Sort.js

--- Algorithms/Selection_Sort.js
+++ Algorithms/Selection_Sort.js
@@ -38,7 +38,6 @@ const selectionSortbtn = document.querySelector(".selectionSort");
 selectionSortbtn.addEventListener('click', async function(){
     disableSortingBtn();
     disableSizeSlider();
-    disableNewArrayBtn();
     await selection();
     enableSortingBtn();
     enableSizeSlider();
