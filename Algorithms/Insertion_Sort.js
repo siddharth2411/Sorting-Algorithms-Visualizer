089b5b1e130a1c27ec771d67b411d4535554983b
siddharth2411 <43139012+siddharth2411@users.noreply.github.com>
2021-09-13 00:00:24 +0530

    Update Insertion_Sort.js

--- Algorithms/Insertion_Sort.js
+++ Algorithms/Insertion_Sort.js
@@ -36,7 +36,6 @@ const inSortbtn = document.querySelector(".insertionSort");
 inSortbtn.addEventListener('click', async function(){
     disableSortingBtn();
     disableSizeSlider();
-    disableNewArrayBtn();
     await insertion();
     enableSortingBtn();
     enableSizeSlider();
